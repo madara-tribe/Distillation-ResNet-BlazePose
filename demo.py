c264883b670266a7fdde2ff6cfcaa5383db5a4bb
madara-tribe <tsukuba.univer@gmail.com>
2022-09-21 17:18:36 +0900

    Distilated Balazepose

--- demo.py
+++ demo.py
@@ -3,28 +3,45 @@ import torch
 import cv2
 
 from models.teacher.blazebase import resize_pad, denormalize_detections
-from models.teacher.blazepose import BlazePose
-from models.teacher.blazepose_landmark import BlazePoseLandmark
+from models.teacher.blazepose import BlazePose as tBlazePose
+from models.teacher.blazepose_landmark import BlazePoseLandmark as tBlazePoseLandmark
 
+from models.student.blazepose import BlazePose as sBlazePose
+from models.student.blazepose_landmark import BlazePoseLandmark as sBlazePoseLandmark
 from utils.visualization import draw_detections, draw_landmarks, draw_roi, POSE_CONNECTIONS
 
-gpu = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-torch.set_grad_enabled(False)
+
+def load_blazepose_net(device, teaher=True, weight=None):
+    if teaher:
+        pose_detector = tBlazePose().to(device)
+        pose_detector.load_weights("src/blazepose.pth")
+        pose_detector.load_anchors("src/anchors_pose.npy")
+
+        pose_regressor = tBlazePoseLandmark().to(device)
+        pose_regressor.load_weights("src/blazepose_landmark.pth")
+        return pose_detector, pose_regressor
+    else:
+        pose_detector = sBlazePose().to(device)
+        if weight:
+            pose_detector.load_state_dict(torch.load(weight, map_location=device))
+        pose_detector.load_anchors("src/anchors_pose.npy")
+
+        pose_regressor = tBlazePoseLandmark().to(device)
+        pose_regressor.load_weights("src/blazepose_landmark.pth")
+        return pose_detector, pose_regressor
+
 
 CAMERA = None
-pose_detector = BlazePose().to(gpu)
-pose_detector.load_weights("src/blazepose.pth")
-pose_detector.load_anchors("src/anchors_pose.npy")
-
-pose_regressor = BlazePoseLandmark().to(gpu)
-pose_regressor.load_weights("src/blazepose_landmark.pth")
+gpu = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+torch.set_grad_enabled(False)
+pose_detector, pose_regressor = load_blazepose_net(gpu, teaher=False, weight="ch/student_iter418000.pth")
 
 WINDOW='test'
 cv2.namedWindow(WINDOW)
 if CAMERA:
     capture = cv2.VideoCapture(0)
 else:
-    capture = cv2.VideoCapture('viz_utils/human_pose.mp4') 
+    capture = cv2.VideoCapture('utils/human_pose.mp4') 
 if capture.isOpened():
     hasFrame, frame = capture.read()
     frame_ct = 0
@@ -34,7 +51,6 @@ while hasFrame:
     frame_ct +=1
     frame = np.ascontiguousarray(frame[:,::-1,::-1])
     img1, img2, scale, pad = resize_pad(frame)
-    print("pad", img1.shape, img2.shape, scale, pad)
     normalized_pose_detections = pose_detector.predict_on_image(img2)
     pose_detections = denormalize_detections(normalized_pose_detections, scale, pad)
 
